c595406085352848e0d712e663071018286bb05e
self067 <seltor@mail.ru>
2020-09-08 00:10:24 +0300

    feat: oauth

---
+++ nocors/index.js
@@ -0,0 +1,8 @@
+
+let { config, app } = require('./server');
+
+let port = process.env.PORT || config.port || 9999;
+
+app.listen(port, null, function (err) {
+  console.log('Gatekeeper, at your service: http://localhost:' + port);
+});
