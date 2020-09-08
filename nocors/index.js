f8f7577870d47d375f7866a778968d9f2004c767
self067 <seltor@mail.ru>
2020-09-08 20:00:23 +0300

    feat:

--- nocors/index.js
+++
@@ -1,8 +0,0 @@
-
-let { config, app } = require('./server');
-
-let port = process.env.PORT || config.port || 9999;
-
-app.listen(port, null, function (err) {
-  console.log('Gatekeeper, at your service: http://localhost:' + port);
-});
