ecd8a645e4a754951620c8d3ca913120f5b1442d
self067 <seltor@mail.ru>
2020-09-13 22:20:54 +0300

    feat: add dbCreateRecord

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Layout, List, Divider,
+  Layout, List, Divider, Button, Input
 } from 'antd';
 import { dbGetReq, dbCreateRecord } from '../../service/restapi-fb';
 
+type tItem = {
+  category: string;
+  description: string;
+  id: string;
+  maxScore: number;
+  minScore: number;
+  title: string;
+
+}
+
 type tTask = {
-  id: number;
+  id: string;
   author: string;
+  name: string;
   state: string;
+  availableToSubmit: boolean;
+  categoriesOrder: string[];
+  items: tItem[];
 };
 
 const {
@@ -22,41 +36,22 @@ type tHome = {
 }
 
 const Home:React.FC<tHome> = ({ logged }) => {
-  const [tasks, setTasks] = useState<[string,tTask][]>([]);
+  const [tasks, setTasks] = useState<[string, tTask][]>([]);
+  const [author, setAuthor] = useState<string>('');
 
   useEffect(() => {
   //  Get tasks from db
-    dbGetReq('tasks', 'author', 'seltor')
+  // dbGetReq('tasks', 'author', 'seltor')  // get only author===seltor
+    dbGetReq('tasks') // get all
       .then((data) => {
         console.log('data', data);
-        // console.log(Object.entries(data.data));
+        console.log(Object.entries(data.data));
         setTasks(Object.entries(data.data));
       })
       .catch(
         (reason:any) => console.error('error', reason)
       );
   }, []);
-
-  ///////// create record 
-  //     dbCreateRecord('tasks',
-  //     {
-  //       author: "newuser",
-  //       categoriesOrder: [ "Basic Scope", "Extra Scope", "Fines" ],
-  //       id: "newtask-v3",
-  //       items: [ {
-  //         category: "Basic Scope",
-  //         description: "You need to make things right, not wrong",
-  //         id: "basic_p1",
-  //         maxScore: 50,
-  //         minScore: 0,
-  //         title: "Basic things"
-  //       }]
-  //     })
-  
-  //     .then((data:any) => {
-  //       console.log('data', data);
-  //     })
-  //     .catch((reason:any) => console.error('error', reason));
 
   return (
     <>
@@ -74,7 +69,7 @@ const Home:React.FC<tHome> = ({ logged }) => {
                   // footer={<div>Footer</div>}
                   bordered
                   dataSource={tasks}
-                  renderItem={ (item:[string, tTask]) => (
+                  renderItem={(item:[string, tTask]) => (
                     <List.Item>
                       {item[0]} {item[1].id} {item[1].author} {item[1].state}
                     </List.Item>
@@ -82,7 +77,48 @@ const Home:React.FC<tHome> = ({ logged }) => {
                 />
               </Content>
             </Layout>
-            <Footer>Footer</Footer>
+
+            <Footer>
+              <Input.Group compact>
+                <Input
+                  addonBefore="author"
+                  value={author}
+                  defaultValue=""
+                  onChange={(e) => { setAuthor(e.target.value); }}
+                />
+              </Input.Group>
+              <Button
+                onClick={() => {
+                  // const 
+                  const newTask:tTask = {
+                    author,
+                    name: 'Task Name',
+                    availableToSubmit: true,
+                    categoriesOrder: ['Basic Scope', 'Extra Scope', 'Fines'],
+                    id: 'reaa-v3',
+                    state: 'DRAFT',
+                    items: [{
+                      category: 'Basic Scope',
+                      description: 'You need to make things right, not wrong',
+                      id: 'basic_p1',
+                      maxScore: 50,
+                      minScore: 0,
+                      title: 'Basic things',
+                    }]
+                  };
+
+                  dbCreateRecord('tasks', newTask)
+                    .then((data) => {
+                      setTasks([...tasks, [data.data.name, newTask]]);
+                    })
+                    .catch(
+                      (reason:any) => console.error('error', reason)
+                    );
+                }}
+              >
+                Добавить task
+              </Button>
+            </Footer>
           </Layout>
         )
         : (
