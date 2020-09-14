17ec61773a3f19f88e51866e1247df9e4bcc53fb
self067 <seltor@mail.ru>
2020-09-14 15:59:53 +0300

    fix: after merge conflict

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,39 +1,19 @@
-/* eslint-disable @typescript-eslint/no-shadow */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../Home';
 import Callback from '../Callback';
 import './app.css';
 
-<<<<<<< HEAD
 
 const App = (props:any) => {
   console.log('app:',props);
   const [logged, setLogged] = useState(true);
-=======
-const Test = () => <div>This is a TEST</div>;
-
-const App = (props: any) => {
-  console.log('app:', props);
-
-  const [logged, setLogged] = useState(false);
->>>>>>> develop
   const [role, setRole] = useState('student');
 
   console.log(logged);
 
   return (
     <Switch>
-<<<<<<< HEAD
-=======
-      <Route path="/test" component={Test} />
-
-      <Route
-        path="/meho"
-        render={(props) => <Home logged setLogged={setLogged} setRole={setRole} props={props} />}
-      />
->>>>>>> develop
       <Route
         path="/callback"
         render={(props) => (
@@ -49,16 +29,12 @@ const App = (props: any) => {
         // exact
         path="/"
         render={(props) => (
-<<<<<<< HEAD
           <Home
             logged={logged}
 //            setLogged={setLogged}
 //            setRole={setRole}
 //            props={props}
           />
-=======
-          <Home logged={logged} setLogged={setLogged} setRole={setRole} props={props} />
->>>>>>> develop
         )}
       />
     </Switch>
@@ -66,3 +42,5 @@ const App = (props: any) => {
 };
 
 export default App;
+
+
