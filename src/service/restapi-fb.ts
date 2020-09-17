f3584b372ca2b78bc5abd6dd24c34b0489eafd90
self067 <seltor@mail.ru>
2020-09-17 16:15:59 +0300

    fix: change restapi-fb to .ts

--- src/service/restapi-fb.ts
+++ src/service/restapi-fb.ts
@@ -4,32 +4,32 @@ const dbUrl = 'https://xcrosscheck.firebaseio.com/';
 
 // let accessToken = '';
 
-export const dbGetReq = (req, index = null, value = null) => {
+export const dbGetReq = (req:string, index:string = null, value:string = null) => {
   const pr = index && value ? `?orderBy="${index}"&equalTo="${value}"` : '';
   const res = axios.get(`${dbUrl}/${req}.json${pr}`); // ,
   // { credentials: 'include', withCredentials: true });
   return res;
 };
 
-export const dbCreateRecord = (req, obj) => {
+export const dbCreateRecord = (req:string, obj) => {
   const res = axios.post(`${dbUrl}/${req}.json`, obj); // ,
   // { credentials: 'include', withCredentials: true });
   return res;
 };
 
-export const dbPutReq = (req, obj) => {
+export const dbPutReq = (req:string, obj) => {
   const res = axios.put(`${dbUrl}/${req}.json`, obj); // ,
   // { credentials: 'include', withCredentials: true });
   return res;
 };
 
-export const dbPatchReq = (req, obj) => {
+export const dbPatchReq = (req:string, obj) => {
   const res = axios.patch(`${dbUrl}/${req}.json`, obj); // ,
   // { credentials: 'include', withCredentials: true });
   return res;
 };
 
-export const dbDeleteReq = (req, key) => {
+export const dbDeleteReq = (req:string, key:string) => {
   const res = axios.delete(`${dbUrl}/${req}/${key}.json`); // ,
   // { credentials: 'include', withCredentials: true });
   return res;
