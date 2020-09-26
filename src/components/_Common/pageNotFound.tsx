9d02646c17b2b98198132e940063f89a0fe28f49
Toffifi <sanyq@mail.ru>
2020-09-26 11:59:05 +0300

    feat: page not found

--- src/components/_Common/pageNotFound.tsx
+++ src/components/_Common/pageNotFound.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Result, Button } from 'antd';
 
 const PageNotFound: FC = () => (
-  <section>
-    <p>Page Not Found</p>
-    <Link to="/">Home</Link>
-  </section>
+  <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button type="primary"><Link to="/">Home</Link></Button>}
+  />
 );
-
 export default PageNotFound;
